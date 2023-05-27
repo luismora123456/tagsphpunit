b47a15e530e02e3ed0a1d2c03880c918262469f9
Luis Mora <lmora@maotsoft.com>
2023-05-26 23:01:41 -0600

    Test http with Data

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,16 +2,25 @@
 
 namespace Tests\Feature;
 
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
+use App\Models\Tag;
 
 class HomeTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testEmpty(): void
+    public function testEmpty()
     {
         $this->get('/')->assertStatus(200)->assertSee('No hay etiquetas');
+    }
+
+    public function testWithData()
+    {
+        $tag = Tag::factory()->create();
+        $this->get('/')->assertStatus(200)->assertSee($tag->name)->assertDontSee('No hay etiquetas');
     }
 }
