daf935b8b754ab186f28073465b67bea5a63664e
Luis Mora <lmora@maotsoft.com>
2023-05-27 00:16:39 -0600

    Add tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,39 +4,53 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        clifford: '#da373d',
+                    }
+                }
+            }
+        }
+    </script>
     <title>Laravel</title>
 
 
 </head>
 
-<body>
-    <form action="tags" method="POST">
-        @csrf
-        <input type="text" name="name" />
-        <input type="submit" value="Agregar" />
-    </form>
-    <h4>Listado de etiquetas</h4>
-    <table>
-        @forelse($tags as $tag)
-        <tr>
-            <td>
-                {{$tag->name}}
-            </td>
-            <td>
-                <form action="tags/{{$tag->id}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" value="Eliminar" />
-                </form>
-            </td>
-        </tr>
-        @empty
-        <tr>
-            <td>No hay etiquetas</td>
-        </tr>
-        @endforelse
-    </table>
+<body class="bg-gray-200 py-10">
+    <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+        <form action="tags" method="POST" class="flex mb-4">
+            @csrf
+            <input type="text" name="name" class="rounded-l bg-gray-200 p-4 w-full outline-none" placeholder="Nueva etiqueta" />
+            <input type="submit" value="Agregar" class="rounded-r px-8 bg-blue-500 text-white outline-none" />
+        </form>
+        <h4 class="text-lg text-center mb-4">Listado de etiquetas</h4>
+        <table>
+            @forelse($tags as $tag)
+            <tr>
+                <td class="border px-4 py-2">
+                    {{$tag->name}}
+                </td>
+                <td class="px-4 py-2">
+                    <form action="tags/{{$tag->id}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Eliminar" class="px-3 rounded bg-red-500 text-white" />
+                    </form>
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td>No hay etiquetas</td>
+            </tr>
+            @endforelse
+        </table>
+    </div>
+
 
 </body>
 
